8dba83231acafaa36837879e810dc064847887b8
yash-mewada <yashmewada27@gmail.com>
2024-02-27 21:09:23 +0530

    Practice 1

---
+++ practice questions/PQ1.java
@@ -0,0 +1,12 @@
+class PQ1{
+	public static void main(String[] args) {
+		int n = 5;
+
+		for (int i = 0; i<n ; i++) {
+			for (int j = 0; j<=i ;j++ ) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
