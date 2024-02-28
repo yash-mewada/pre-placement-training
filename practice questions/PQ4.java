23f8303d3ba06b3f1e6dab1ed1782de7a92cc401
yash-mewada <yashmewada27@gmail.com>
2024-02-28 20:47:33 +0530

    practice 2

---
+++ practice questions/PQ4.java
@@ -0,0 +1,14 @@
+class PQ4 {
+	public static void main(String[] args) {
+		int n = 5;
+
+		for (int i = 1; i <= n ; i++) {
+			for (int j = 1 ;j <= n ;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}	
+
+	
