b89fb7efd23e2c6f81f4b0918bd5af7c0f7cf9f9
yash-mewada <yashmewada27@gmail.com>
2024-03-05 21:02:34 +0530

    practice

---
+++ practice questions/PQ8.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class PQ8 {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        int count = 0;
+        
+        while(sc.hasNextLine()){
+            count++;
+            System.out.println(count + " " + sc.nextLine());
+        }
+    }
+}
