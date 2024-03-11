49236f7cc9974d75b094ed35f5c35ad8661fd0cd
yash-mewada <yashmewada27@gmail.com>
2024-03-11 20:36:04 +0530

    assignment

---
+++ assignment3/A1.java
@@ -0,0 +1,13 @@
+class A1 {
+	public static void main(String[] args) {
+		int n = 65;
+
+		for (int i = 1;i <= 4 ;i++ ) {
+			for (int j = 1;j <= 5 ;j++ ) {
+				System.out.print((char)n);
+			}
+			n++;
+			System.out.println();
+		}
+	}
+}
---
+++ assignment3/A2.java
@@ -0,0 +1,14 @@
+class A2 {
+	public static void main(String[] args) {
+		
+
+		for (int i = 1;i <= 4 ;i++ ) {
+			int n = 65;
+			for (int j = 1;j <= 5 ;j++ ) {
+				System.out.print((char)n);
+				n++;
+			}
+			System.out.println();
+		}
+	}
+}
