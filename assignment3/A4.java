34a1c3b85be99cbd362d9f8d1d0cfa6e10c1b5fb
yash-mewada <yashmewada27@gmail.com>
2024-03-13 20:42:21 +0530

    assignment

---
+++ assignment3/A4.java
@@ -0,0 +1,10 @@
+class A4{
+	public static void main(String[] args) {
+		for (int i = 1;i <= 10 ;i++ ) {
+			for (int j = 10;j >= 1 ;j--) {
+				System.out.print(j + " ");
+			}
+			System.out.println();
+		}
+	}
+}
